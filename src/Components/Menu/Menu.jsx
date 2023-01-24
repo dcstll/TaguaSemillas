4abff973c3915b1f148f767030614e094c2004b4
dcstll <danicastillo789@gmail.com>
2023-01-23 22:45:00 -0500

    NavBar Styles

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -3,39 +3,43 @@ import { NavLink } from 'react-router-dom';
 import { AiFillShopping } from 'react-icons/ai';
 import { useShoppingCart } from '../../context/ShoppingCartContext';
 import logo from "../../assets/sin-fondo.png"
+import style from "./Menu.module.css"
 
 function Menu() {
   const { openCart, cartQuantity } = useShoppingCart()
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" className={style.nav}>
       <Container>
-        <Navbar.Brand>
-          <img src={logo} alt="Logo" className="w-25"/>
+        <Navbar.Brand className={style.brand}>
+          <div>
+          <img src={logo} alt="Logo" className={style.imgLogo}/>
+          </div>
           Tagua y Semillas</Navbar.Brand>
        <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav"> 
-          <Nav className="me-auto">
-            <Nav.Link to="/" as={NavLink} >Inicio</Nav.Link>
-            <Nav.Link to="/about" as={NavLink}>Sobre Nosotros</Nav.Link>
-            <Nav.Link to="/Catalogue" as={NavLink}>Catálogo</Nav.Link>
+        <Navbar.Collapse id="basic-navbar-nav" className={style.links}> 
+          <Nav>
+            <Nav.Link to="/" as={NavLink} className={style.link}>Inicio</Nav.Link>
+            <Nav.Link to="/about" as={NavLink} className={style.link}>Sobre Nosotros</Nav.Link>
+            <Nav.Link to="/Catalogue" as={NavLink} className={style.link}>Catálogo</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-        {cartQuantity > 0 && (<Button onClick={openCart} style={{width: "3rem", height: "3rem", position: "relative"}} variant="outline-primary" className="rounded-circle">
+        <button onClick={openCart} className={style.cartButton}>
           <AiFillShopping size={28}/>
-          <div className='rounded-circle bg-danger d-flex justify-content-center align-items-center' style={{ 
+          {cartQuantity > 0 && (<div className='rounded-circle bg-success d-flex justify-content-center align-items-center' style={{ 
             color: "white", 
             width: "1.5rem", 
             height: "1.5rem", 
             position: "absolute",
             bottom: 0,
             right: 0,
-            transform: "translate(12%, 12%)"
+            transform: "translate(40%, 40%)"
 
             }}>{cartQuantity}
             </div>
-        </Button>)}
+            )}
+        </button>
       </Container>
     </Navbar>
   );
